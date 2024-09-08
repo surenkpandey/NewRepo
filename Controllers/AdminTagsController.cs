6940f4ffd2b583329d1bcf99eae6c958da330d13
Suren Pandey <suren.kpandey@gmail.com>
2024-09-08 12:01:42 +0530

    Add project files.

---
+++ Controllers/AdminTagsController.cs
@@ -0,0 +1,39 @@
+﻿using Blog.Data;
+using Blog.Models.ViewModels;
+using Blog.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+	public class AdminTagsController : Controller
+	{
+        private readonly BlogDbContext blogDbContext;
+
+        public AdminTagsController(BlogDbContext blogDbContext)
+        {
+            this.blogDbContext = blogDbContext;
+        }
+        [HttpGet]
+		public IActionResult Add()
+		{
+			return View();
+		}
+		
+		[HttpPost]
+		[ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            // Mapping AddTagRequest to get Tag domain model
+            var tag = new Tag
+            {
+            Name = addTagRequest.Name,
+            DisplayName = addTagRequest.DisplayName,
+            };
+
+            blogDbContext.Tags.Add(tag);
+            blogDbContext.SaveChanges();
+            return View("Add");
+        }
+
+    }
+}
---
+++ Data/BlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
